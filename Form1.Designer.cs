f7efc8d2cf667723dada35220e1bef309f08bb71
Mehr Muhammad Hamza <mhamzap10@gmail.com>
2021-07-16 09:04:41 -0700

    All features added. Tutorial is complete

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace Create_Pdf_using_IronPdf
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.PdfText = new System.Windows.Forms.RichTextBox();
+            this.Save = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Close = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(236, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(440, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "C# Create Pdf Using IronPdf";
+            // 
+            // PdfText
+            // 
+            this.PdfText.Location = new System.Drawing.Point(30, 100);
+            this.PdfText.Name = "PdfText";
+            this.PdfText.Size = new System.Drawing.Size(882, 370);
+            this.PdfText.TabIndex = 1;
+            this.PdfText.Text = "";
+            this.PdfText.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Save
+            // 
+            this.Save.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Save.Location = new System.Drawing.Point(215, 491);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(116, 33);
+            this.Save.TabIndex = 2;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Clear.Location = new System.Drawing.Point(413, 491);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(116, 33);
+            this.Clear.TabIndex = 3;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(261, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(395, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Write Your Text here (Use Html Tages for Formatting)";
+            // 
+            // Close
+            // 
+            this.Close.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Close.Location = new System.Drawing.Point(609, 491);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(116, 33);
+            this.Close.TabIndex = 6;
+            this.Close.Text = "Close";
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 550);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.PdfText);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Create Pdf Using IronPdf";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox PdfText;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Close;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using IronPdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Create_Pdf_using_IronPdf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            // Code for Select the folder to save the file.
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.InitialDirectory = @"D:\";      
+            saveFileDialog1.Title = "Save Pdf File";
+            saveFileDialog1.DefaultExt = "pdf";
+            saveFileDialog1.Filter = "Pdf files (*.pdf)|*.pdf|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filename = saveFileDialog1.FileName;
+                // actual code that will create Pdf files
+                var HtmlLine = new HtmlToPdf();
+                HtmlLine.RenderHtmlAsPdf(PdfText.Text).SaveAs(filename);
+                // MessageBox to display that file save
+                MessageBox.Show("File Saved Successfully!");
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            PdfText.Text = "";
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+    }
+}
